8422ac7b129de1bf5d8f1889639ce7e0b6f43f7a
bit-ksw <bit7521@gmail.com>
2019-04-12 23:03:20 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,23 @@ const styles = theme => ({
   }
 });
 
-const customer = [
-  {
-    id: 1,
-    image: "http://placeimg.com/64/64/1",
-    name: "홍길동",
-    birthday: "961222",
-    gender: "남자",
-    job: "대학생"
-  },
-  {
-    id: 2,
-    image: "http://placeimg.com/64/64/2",
-    name: "ksw",
-    birthday: "961222",
-    gender: "남자",
-    job: "대학생"
-  },
-  {
-    id: 3,
-    image: "http://placeimg.com/64/64/3",
-    name: "ksh",
-    birthday: "961222",
-    gender: "남자",
-    job: "대학생"
-  }
-];
-
 class App extends Component {
+  state = {
+    customers: ""
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,19 +53,21 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customer.map(c => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
+            {this.state.customers
+              ? this.state.customers.map(c => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
