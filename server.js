e3544997d427220263d2f2ed4b6585cd4c4e4b2d
bit-ksw <bit7521@gmail.com>
2019-04-16 21:26:21 +0900

    Add Customer Delete Module

--- server.js
+++ server.js
@@ -24,8 +24,10 @@ connection.connect();
 const multer = require('multer');
 const upload = multer({ dest: './upload' });
 
+app.use('/image/', express.static('./upload'));
+
 app.post('/api/customers', upload.single('image'), (req, res) => {
-  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+  let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
   let image = '/image/' + req.file.filename;
   let name = req.body.name;
   let birthday = req.body.birthday;
@@ -40,11 +42,20 @@ app.post('/api/customers', upload.single('image'), (req, res) => {
 });
 
 app.get("/api/customers", (req, res) => {
-  connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+  connection.query("SELECT * FROM CUSTOMER WHERE isDeleted = 0", (err, rows, fields) => {
     res.send(rows);
   });
 });
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'UPDATE CUSTOMER SET isDeleted = 1 WHERE id = ?';
+  let params = [req.params.id];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  )
 
-app.use('/image/', express.static('./upload'));
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
